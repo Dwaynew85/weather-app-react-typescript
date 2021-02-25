d4bcc397411cdcd184c5e4dd0a65fc0a545fea6e
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-25 13:34:38 -0500

    Added location input and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [locationSearch, setLocation] = useState('Paris');
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <div>
+        <label>
+          Add Location <input type="text" value="Paris"/>
+        </label>
+        <button>Search</button>
+      </div>
+
+      <div>
+        <h2>Location</h2>
+        <table>
+          <thead>
+          <tr>
+            <th>Name</th>
+          </tr>
+          </thead>
+          <tbody>
+            <tr><td>Belfast</td></tr>
+            <tr><td>New York</td></tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
