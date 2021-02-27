fa488bee0d43da8a40dd85943c36aa3bbcf38fa7
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-02-27 03:55:08 -0500

    Added LocationTable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {LocationSearch} from './LocationSearch';
+import {LocationTable} from './LocationTable'
 import './App.css';
 
 function App() {
@@ -9,24 +10,8 @@ function App() {
   return (
     <div className="container">
       <h1>Weather App</h1>
-
-      <LocationSearch onSearch={addLocation}/>
-
-      <div>
-        <h2>Locations</h2>
-        <table className="table table-hover" >
-          <thead>
-          <tr>
-            <th>Name</th>
-          </tr>
-          </thead>
-          <tbody>
-            {locations.map((location, index) => <tr key={index}><td>{location}</td></tr>)}
-            <tr><td>Belfast</td></tr>
-            <tr><td>New York</td></tr>
-          </tbody>
-        </table>
-      </div>
+      <LocationSearch onSearch={addLocation}/> 
+      <LocationTable locations={locations}/>     
     </div>
   );
 }
