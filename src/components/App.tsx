661c148e0810e54ba34d7960349ab3f1f6664c04
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-03-03 02:47:35 -0500

    Added error and warning states, modified addLocation, and function to reset Alerts

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,40 @@
-import React, {useState} from 'react';
-import {LocationSearch} from './LocationSearch';
-import {LocationTable} from './LocationTable'
+import React, {FC, useState} from 'react';
 import './App.css';
+import {LocationSearch} from './LocationSearch';
+import {LocationTable} from './LocationTable';
+import {WeatherLocation} from '../model/Weather';
+import {ErrorAlert, WarningAlert} from './Alert';
+import {searchLocation} from "../services/WeatherService"
 
-function App() {
-  const [locations, setLocations] = useState<string[]>([]);  
-  const addLocation = (location: string) => setLocations([location, ...locations]);
+const App: FC = () => {
+  const [locations, setLocations] = useState<WeatherLocation[]>([])
+  const [error, setError] = useState('');
+  const [warning, setWarning] = useState('');
+
+  const resetAlerts = () => {
+    setError('');
+    setWarning('');
+  }
+
+  let addLocation = async (term: string) => {
+    resetAlerts();
+    const location = await searchLocation(term);
+
+    if (!location) {
+      setError(`No location found called '${term}'`);
+    } else if (locations.find(item => item.id === location.id)) {
+      setWarning(`Location '${term} is already in the list.`);
+    } else {
+      setLocations([location, ...locations]);
+    }
+  };
 
   return (
     <div className="container">
       <h1>Weather App</h1>
       <LocationSearch onSearch={addLocation}/> 
+      <ErrorAlert message={error}/>
+      <WarningAlert message={warning}/>
       <LocationTable locations={locations}/>     
     </div>
   );
