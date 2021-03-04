64f3b2cda57a40ecea3efe672c68979ec03b13f3
Dwaynew85 <mr_dwayne_w@yahoo.com>
2021-03-04 04:46:21 -0500

    Added Weather Summary component displays weather information

---
+++ src/components/WeatherSummary.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useEffect, useState } from "react";
+import { Weather, WeatherLocation } from "../model/Weather";
+import { readWeather } from "../services/WeatherService";
+import { WeatherEntry } from "./WeatherEntry";
+
+
+interface WeatherSummaryProps {
+    location: WeatherLocation | null;
+}
+
+export const WeatherSummary: FC<WeatherSummaryProps> = ({location}) => {
+    const [weather, setWeather] = useState<Weather | null>(null);
+
+    useEffect(() => {
+        if (location) {
+            readWeather(location.id).then(weather => setWeather(weather));
+        }
+    }, [location]);
+
+    if (!location || !weather) return null;
+
+    return (
+        <div>
+            <hr/>
+            <h2>{location.name}</h2>
+            <WeatherEntry weather={weather}/>
+        </div>
+    )
+};
